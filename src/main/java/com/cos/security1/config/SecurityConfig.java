5166d81f20bd7c0cbacc8ace771608c77f60ee60
Juhyeok Kim <juhyeok.dev@gmail.com>
2021-08-12 17:08:42 +0900

    시큐리티 로그인 처리

--- src/main/java/com/cos/security1/config/SecurityConfig.java
+++ src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -26,6 +26,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
-                .loginPage("/loginForm");
+                .loginPage("/loginForm")
+                .loginProcessingUrl("/login") // /login 주소가 호출이 되면 시큐리티가 낚아채서 대신 로그인을 진행한다. (Controller에 login method를 만들지 않아도 된다.)
+                .defaultSuccessUrl("/");
     }
 }
---
+++ src/main/java/com/cos/security1/config/auth/PrincipalDetails.java
@@ -0,0 +1,69 @@
+package com.cos.security1.config.auth;
+
+//시큐리티가 /login 주소 요청이 오면 낚아채서 로그인을 진행한다.
+//로그인 진행이 완료가 되면 시큐리티 session을 만든다.(SecurityContextHolder)
+//오브젝트 = Authentication 타입 객체
+//Authentication 안에 User 정보가 있어야 한다.
+//User 오브젝트의 타입은 UserDetails 타입 객체
+/* Security Session => Authentication => UserDetails(PrincipalDetails) */
+
+import com.cos.security1.model.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PrincipalDetails implements UserDetails {
+
+    private User user; //Composition
+
+    public PrincipalDetails(User user) {
+        this.user = user;
+    }
+
+    //해당 User의 권한을 리턴하는 곳
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return user.getRole();
+            }
+        });
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        //false가 되는 경우
+        //1. 휴면계정으로 전환
+        return true;
+    }
+}
